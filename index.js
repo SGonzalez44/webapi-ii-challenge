07d8d62ea24241dc4cef17342eb90be2937d9092
salvador <salgonzolanda@gmail.com>
2019-10-04 00:13:03 -0700

    set up my index .js

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const server = express();
+
+//Routes
+const postRoutes = require('./routes/posts.js')
+
+//Middleware
+server.use(express.json());
+
+server.use('/api/posts', postRoutes);
+
+server.get('/', (req, res) => {
+    res.send('Server running from Sals mind');
+});
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n * Server Running on http://localhost:${port} * \n`);  
+});
